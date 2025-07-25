0773d7f63432adabc8cdf605d2fcad9d4c1f1fed
Benoît Rouits <brouits@free.fr>
2025-07-26 00:13:07 +0200

    Fix app icon display on Desktop Manager

--- main.cpp
+++ main.cpp
@@ -9,8 +9,6 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    a.setOrganizationName("Herewe");
-    a.setOrganizationDomain("Servebeer");
     a.setApplicationName("QLiquidSFZ");
     a.setApplicationVersion(VERSION " (" REVISION ")");
 
